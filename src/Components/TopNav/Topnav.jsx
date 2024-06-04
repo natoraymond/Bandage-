617a480b2ae2e312f2ab548a31e92bd7521b2614
Your Name <nnamdi.raymondoct@gmail.com>
2024-06-04 21:36:26 +0100

    update on hero section

--- src/Components/TopNav/Topnav.jsx
+++ src/Components/TopNav/Topnav.jsx
@@ -1,38 +1,34 @@
 import React from 'react'
 import styleA from '../TopNav/Topnav.module.css'
-import phonelogo from '../../../public/images/Phonelogo.png'
-import email from  '../../../public/images/email.png'
-import facebok from  '../../../public/images/facebk.png'
-import insta from  '../../../public/images/insta.png'
-import youtube from  '../../../public/images/youtube.png'
-import x from  '../../../public/images/x.png'
+
+
 const Topnav = () => {
   return (
     <div className={styleA.contain}>
         <div className={styleA.topsideA}>
           <div className={styleA.mostphone}>
-            <img src={phonelogo}/>
+            <img src="/images/navbartop-pic/phone.png" alt="" />
             <div className={styleA.phonNumber}>(225) 555-0118</div>
           </div>
 
           <div className={styleA.emailside}>
-            <img src={email}/>
+          <img src="/images/navbartop-pic/email.png" alt="" />
             <div className={styleA.emailText}>michelle.rivera@example.com</div>
           </div>
 
         </div>
 
-        {/* side two */}
+        {/* centre section */}
         <div className={styleA.topsideB}>Follow Us  and get a chance to win 80% off</div>
        
-         {/* side three */}
+         {/* right icone */}
         <div className={styleA.topsideC}>
           <div className={styleA.follow}>Follow Us  :</div>
           <div className={styleA.topicon}>
-            <img className={styleA.ins} src={insta}/>
-            <img src={youtube}/>
-            <img src={facebok}/>
-            <img src={x}/>
+          <img src="/images/navbartop-pic/instagram.png" alt="" />
+            <img src="/images/navbartop-pic/youtube.png" alt="" />
+            <img src="/images/navbartop-pic/facebook.png" alt="" />
+            <img src="/images/navbartop-pic/twitter.png" alt="" />
           </div>
         </div>
 
